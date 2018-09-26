ddc114fdd73622c283adbe0841c8351d6b2769be
pofider <jan.blaha@hotmail.com>
2018-09-26 17:01:10 +0200

    download binary from github

---
+++ jsreport.Binary.OSX/download.ps1
@@ -0,0 +1,7 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-osx.tar.gz','jsreport-osx.tar.gz')
+7z x "jsreport-osx.tar.gz" -so | 7z x -aoa -si -ttar
+ren jsreport jsreport.exe
+Compress-Archive -LiteralPath jsreport.exe  -CompressionLevel Optimal -DestinationPath jsreport.zip 
+del jsreport.exe
+del jsreport-osx.tar.gz
