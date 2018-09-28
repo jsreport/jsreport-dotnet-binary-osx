60100c6f72e6a7103d280ab21eba0e987d023c03
pofider <jan.blaha@hotmail.com>
2018-09-28 14:48:59 +0200

    download binary

---
+++ jsreport.Binary.OSX/download.sh
@@ -0,0 +1,11 @@
+﻿#download the binary from github and zip it as expected by c#
+wget https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-osx.tar.gz 
+tar -xzf jsreport-osx.tar.gz 
+mv jsreport jsreport.exe 
+zip jsreport-new.zip ./jsreport.exe
+mv jsreport-new.zip jsreport.zip
+rm jsreport-osx.tar.gz
+rm license.txt
+rm jsreport.exe
+
+# the test in travis unfortunatelly still fails on version `GLIBCXX_3.4.20' not found 
