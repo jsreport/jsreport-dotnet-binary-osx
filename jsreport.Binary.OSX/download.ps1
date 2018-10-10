538be49f4e7cb56fe592c1148725151df58beda8
pofider <jan.blaha@hotmail.com>
2018-10-10 16:00:12 +0200

    release 2.2.1

--- jsreport.Binary.OSX/download.ps1
+++ jsreport.Binary.OSX/download.ps1
@@ -1,7 +1,8 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 (new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-osx.tar.gz','jsreport-osx.tar.gz')
-7z x "jsreport-osx.tar.gz" -so | 7z x -aoa -si -ttar
+& cmd.exe '/C 7z x "jsreport-osx.tar.gz" -so | 7z x -aoa -si -ttar'
 ren jsreport jsreport.exe
-Compress-Archive -LiteralPath jsreport.exe  -CompressionLevel Optimal -DestinationPath jsreport.zip 
+Compress-Archive -LiteralPath jsreport.exe  -CompressionLevel Optimal -DestinationPath jsreport.zip -Force
 del jsreport.exe
 del jsreport-osx.tar.gz
+del license.txt
