3ac46c55fba06a5ae435b1a10a27682d9fbbfaae
pofider <honza.pofider@seznam.cz>
2022-11-23 22:04:21 +0100

    release 3.9.0

--- jsreport.Binary.OSX/download.ps1
+++ jsreport.Binary.OSX/download.ps1
@@ -1,5 +1,5 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/2.9.0/jsreport-osx.tar.gz','jsreport-osx.tar.gz')
+(new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/3.9.0/jsreport-osx.tar.gz','jsreport-osx.tar.gz')
 & cmd.exe '/C 7z x "jsreport-osx.tar.gz" -so | 7z x -aoa -si -ttar'
 ren jsreport jsreport.exe
 Compress-Archive -LiteralPath jsreport.exe  -CompressionLevel Optimal -DestinationPath jsreport.zip -Force
